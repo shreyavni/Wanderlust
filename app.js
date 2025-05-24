482c075938a4de129b1d476b205444c079a34245
Avni Shukla <avnireeta@gmail.com>
2025-05-25 00:42:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -80,9 +80,9 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-// });
+app.get('/', (req, res) => {
+    res.redirect("/listings");
+});
 
 app.use((req,res,next)=>{
     res.locals.currUser = req.user;
